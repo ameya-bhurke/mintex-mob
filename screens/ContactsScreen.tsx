63ea34b7b84b40d4e7605654298b2805cf5e8f75
Ameya Bhurke <ameyabhurke@gmail.com>
2022-02-10 21:05:35 +1100

    Login screen dummy impl

---
+++ screens/ContactsScreen.tsx
@@ -0,0 +1,9 @@
+import { View } from 'react-native'
+import { Text } from 'react-native-paper'
+import { ContactsScreenNavigationProp } from './types'
+
+export const ContactsScreen = (navigation: ContactsScreenNavigationProp) => {
+    return <View>
+        <Text>Contacts</Text>
+    </View>
+}
