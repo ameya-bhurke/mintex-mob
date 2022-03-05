dd1a1fb6aa7586ee5e6093f0468baa673951e73a
Ameya Bhurke <ameyabhurke@gmail.com>
2022-03-06 08:41:04 +1100

    missed to add file

---
+++ screens/MessagesScreen.tsx
@@ -0,0 +1,44 @@
+import { StyleSheet, Text, View } from 'react-native'
+import { Message } from '../components/Message'
+import { useStores } from '../stores/stores'
+import { MessagesScreenProps } from './types'
+
+export const MessagesScreen = (navigation: MessagesScreenProps) => {
+    const { contactStore, messageStore } = useStores()
+
+    
+    const messages  = contactStore.getAll().map(contact => {
+        if(messageStore.getMessageListFor(contact).length == 0) return null
+        else { 
+            const msg = messageStore.getMessageListFor(contact)[0]
+            return <Message  
+            id={contact.name}
+            content={msg.content}
+            date={msg.time}
+            direction={msg.direction}
+        />}})
+
+    return <View style={styles.container}>
+        <View>
+            {messages}
+        </View>
+    </View>
+}
+
+const backgroundColor = 'white'
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'stretch',
+      justifyContent: 'flex-start',
+      borderLeftWidth: 10,
+      borderLeftColor: backgroundColor,
+      borderTopWidth: 10,
+      borderTopColor: backgroundColor,
+      borderRightWidth: 10,
+      borderRightColor: backgroundColor,
+      borderBottomWidth: 10,
+      borderBottomColor: backgroundColor,
+    },
+});
